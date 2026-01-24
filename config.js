635bc5c5b9816a130f2466e48095607b26a7c041
anonymousofficial26 <t.0717237343@gmail.com>
2026-01-24 21:52:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 export default {
-  SESSION_ID: "PASTE_SESSION_HERE",
-  botName: "NovaX-MD",
-  prefix: "."
+  prefix: ".",
+
+  // 🔐 PASTE BASE64 SESSION HERE
+  SESSION_ID: "PASTE_YOUR_BASE64_SESSION_STRING_HERE",
+
+  owner: ["263XXXXXXXX"],
+  botName: "NovaX-MD"
 }
